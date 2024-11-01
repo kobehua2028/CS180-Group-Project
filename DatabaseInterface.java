3488566426778b944c12c381d4e96dfe9a332b61
Michael Ikriannikov <mikriann@purdue.edu>
2024-11-01 18:04:20 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -7,26 +7,23 @@ public interface DatabaseInterface {
       ArrayList<Comment> comments //All comments that exist on the platform, including replies.
     *
     *
-    *
-    *
-    *
     *  */
 
     //The following three methods are inspired by readResearcher() from PRJ-3
-    boolean readUsers(String usersFile); //File containing all users that initially exist
-    boolean readPosts(String postsFile); //File containing all posts that initially exist
-    boolean readComments(String commentsFile); //File containing all comments that initially exist.
+    //boolean readUsers(String usersFile); //File containing all users that initially exist
+    //boolean readPosts(String postsFile); //File containing all posts that initially exist
+    //boolean readComments(String commentsFile); //File containing all comments that initially exist.
 
 
-    boolean createNewUser(String username, String password); //A method that creates a new user
+    //boolean createNewUser(String username, String password); //A method that creates a new user
                                                     // if the username and password exist.
 
     //Ideally, the three methods would be triggered by User, Post, and Comment objects
-    boolean deleteUser(User markedForDeletion); //Removes user from database
-    boolean deletePost(Post markedForDeletion); //Removes post from database
-    boolean deleteComment(Comment comment); //Removes comment from database
+    //boolean deleteUser(User markedForDeletion); //Removes user from database
+    //boolean deletePost(Post markedForDeletion); //Removes post from database
+    //boolean deleteComment(Comment comment); //Removes comment from database
 
-    ArrayList<Post> newFeed(User user); //returns a list of posts in reverse chronological order.
+    //ArrayList<Post> newFeed(User user); //returns a list of posts in reverse chronological order.
                                         //Filters posts by blocked users.
                                         //Prioritizes posts by friends?
     
