b2f12728f002e8bd39d445842f5f68571aee662a
AlQarni-37 <abdulmajedalqarni212@gmail.com>
2024-11-01 17:46:36 -0400

    Add files via upload
    What I have so far for Comment.java

---
+++ Comment.java
@@ -0,0 +1,25 @@
+public class Comment {
+    private String text;
+    private int likes;
+    private int dislikes;
+    private User author;
+    private Post post; //the post to which this comment belongs
+    private SocialMediaDatabase sm;
+
+
+    public Comment(String commentLine, SocialMediaDatabase sm) {
+        this.sm = sm;
+
+        author = sm.findUser(commentLine.substring(0, commentLine.indexOf(",")));
+        commentLine = commentLine.substring(commentLine.indexOf(",") + 1);
+
+        post = sm.findPost(commentLine.substring(0, commentLine.indexOf(",")));
+        commentLine = commentLine.substring(commentLine.indexOf(",") + 1);
+
+        text = commentLine;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
