4943b7588d32d33e9ea03b44520410e483c62598
Levin Cozza <148361840+levincozza@users.noreply.github.com>
2024-11-02 17:45:31 -0400

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -1,25 +1,38 @@
-public class Comment {
+import java.io.Serializable;
+
+public class Comment implements Serializable {
     private String text;
     private int likes;
     private int dislikes;
     private User author;
     private Post post; //the post to which this comment belongs
-    private SocialMediaDatabase sm;
 
-
-    public Comment(String commentLine, SocialMediaDatabase sm) {
-        this.sm = sm;
-
-        author = sm.findUser(commentLine.substring(0, commentLine.indexOf(",")));
-        commentLine = commentLine.substring(commentLine.indexOf(",") + 1);
-
-        post = sm.findPost(commentLine.substring(0, commentLine.indexOf(",")));
-        commentLine = commentLine.substring(commentLine.indexOf(",") + 1);
-
-        text = commentLine;
+    public Comment(User author, String text, int likes, int dislikes, Post post) {
+        this.author = author;
+        this.text = text;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        this.post = post;
+        post.addComment(this);
     }
 
     public String getText() {
         return text;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Post getPost() {
+        return post;
     }
 }
