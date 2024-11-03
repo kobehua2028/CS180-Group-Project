1c85c3d5baf4fb54f4a17db0c5856a98f61231e3
Michael Ikriannikov <mikriann@purdue.edu>
2024-11-03 13:51:04 -0500

    Update PostInterface.java

--- PostInterface.java
+++ PostInterface.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
-
+/**
+ * CS180 Group Project
+ * Program description here
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2024</p>
+ *
+ * @author
+ * @version Nov 03, 2024
+ */
 public interface PostInterface {
     void createComment(User author, String text);
     boolean equals(Post post);
