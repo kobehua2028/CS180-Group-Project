ad7fce92f47fbd72f7c745d27f6ca9a19fe4251e
Michael Ikriannikov <mikriann@purdue.edu>
2024-11-03 15:33:27 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class User implements Serializable {
+public class User implements Serializable, UserInterface {
     private ArrayList<User> friendsList = new ArrayList<User>(); //list of users that are friends/followed by this user
     private ArrayList<User> blockedList = new ArrayList<User>(); //list of users that are blocked by this user
     private final String username; //the name of this account
