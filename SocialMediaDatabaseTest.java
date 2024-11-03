ee13f5c7a915882a79bbca948c2b976c2d7b01c7
kobehua2028 <kobehuang2028@gmail.com>
2024-11-03 14:09:32 -0500

    Update SocialMediaDatabaseTest.java

--- SocialMediaDatabaseTest.java
+++ SocialMediaDatabaseTest.java
@@ -13,15 +13,47 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class SocialMediaDatabaseTest {
     private SocialMediaDatabase sm = new SocialMediaDatabase("users.dat", "posts.dat");
+    private User expectedUser = new User("Dunsmore","CS180istheBest",
+            "I teach CS180", new ArrayList<>(), new ArrayList<>(), sm);
+    private Post expectedPost = new Post(expectedUser, "Purdue CS180",
+            "Purdue CS 180 is the best CS class", new ArrayList<Comment>(), 0, 0, sm);
+    private ArrayList<User> expected = new ArrayList<>();
 
     @Test
     public void testCreateUser() {
-        sm.createUser("Professor Dunsmore","CS180istheBest", "I teach CS180");
-
-        ArrayList<User> expected = new ArrayList<>();
-        expected.add(new User("Professor Dunsmore","CS180istheBest",
-                "I teach CS180", new ArrayList<>(), new ArrayList<>(), sm));
+        sm.createUser("Dunsmore","CS180istheBest", "I teach CS180");
+        expected.add(expectedUser);
         assertEquals(expected, sm.getUsers());
+    }
+
+    @Test
+    public void testFindUser() {
+        if (!sm.getUsers().contains(expectedUser)) {
+            sm.createUser("Dunsmore","CS180istheBest", "I teach CS180");
+        }
+        User foundUser = sm.findUser("Dunsmore");
+        assertEquals(expectedUser, foundUser);
+    }
+
+    @Test
+    public void testUserExists() {
+        if (!sm.getUsers().contains(expectedUser)) {
+            sm.createUser("Dunsmore","CS180istheBest", "I teach CS180");
+        }
+        boolean exists = sm.userExists("Dunsmore");
+        assertTrue(exists);
+    }
+
+    @Test
+    public void testFindPost() {
+        if (!sm.getPosts().contains(expectedPost)) {
+            if (!sm.getUsers().contains(expectedUser)) {
+                sm.createUser("Dunsmore","CS180istheBest", "I teach CS180");
+            }
+            sm.getUsers().getFirst().createPost("Purdue CS180", "Purdue CS 180 is the best CS class");
+        }
+        Post foundPost = sm.findPost("Purdue CS180");
+        assertEquals(expectedPost, foundPost);
     }
 
     @Test
@@ -29,24 +61,11 @@ public class SocialMediaDatabaseTest {
         sm.createUser("PurduePete", "MyPurdue1234", "Hey guys it's me, Pete from Purdue!");
         sm.readUsers();
         ArrayList<User> expected = new ArrayList<>();
-        expected.add(new User("Professor Dunsmore","CS180istheBest",
+        expected.add(new User("Dunsmore","CS180istheBest",
                 "I teach CS180", new ArrayList<>(), new ArrayList<>(), sm));
         expected.add(new User("PurduePete", "MyPurdue1234",
                 "Hey guys it's me, Pete from Purdue!", new ArrayList<>(), new ArrayList<>(), sm));
         assertEquals(expected, sm.getUsers());
     }
-
-    @Test
-    public void testReadPosts() {
-        sm.readUsers();
-        sm.readPosts();
-
-        ArrayList<Post> expected = new ArrayList<>();
-        expected.add(new Post
-                ("PurduePete,Project Help,Hey guys, I'm having a hard time figuring out the project.", sm));
-
-        assertEquals(true, sm.getPosts().get(0).equals(expected.get(0)));
-    }
-
 
 }
