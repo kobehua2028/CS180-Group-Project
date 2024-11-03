42f19347a4cef9e39d4083ff5d31944b28f1a22f
Levin Cozza <148361840+levincozza@users.noreply.github.com>
2024-11-03 13:30:43 -0500

    Update PostInterface.java

--- PostInterface.java
+++ PostInterface.java
@@ -1,39 +1,15 @@
 import java.util.ArrayList;
 
-
 public interface PostInterface {
-    /* Fields:
-    static int commentCount
-    static int likeCount
-    static int dislikeCount  //static because they might be modified by multiple users simultaneously
-    User originalPoster //The user who made this post
-    String title //title of post
-    ArrayList<Comment> comments //List of comments left by users.
-        //Should the poster be able to delete comments on their post?
-    *
-    *
-    *
-    *
-    * */
     void createComment(User author, String text);
-    
     boolean equals(Post post);
-    
-    void addComment(Comment comment, SocialMediaDatabase sm);
-    
+    void addComment(Comment comment);
     String getTitle();
-    
     String getSubtext();
-    
     User getAuthor();
-    
     ArrayList<Comment> getComments();
-    
     int getLikes();
-    
     int getDislikes();
-    
     void incrementLikes();
-
-    public void incrementDislikes();
+    void incrementDislikes();
 }
