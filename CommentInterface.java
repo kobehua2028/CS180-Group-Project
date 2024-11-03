031ed9d30909b3832a4300babad9d4808f931152
levin <levincozza@gmail.com>
2024-11-03 13:28:03 -0500

    Made interface for comment class

--- CommentInterface.java
+++ CommentInterface.java
@@ -1,3 +1,9 @@
 public interface CommentInterface {
-    //
+    String getText();
+    int getLikes();
+    int getDislikes();
+    User getAuthor();
+    Post getPost();
+    void incrementLikes();
+    void incrementDislikes();
 }
