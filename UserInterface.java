3e482d755db91582fc465c80bd53e1814976b409
Michael Ikriannikov <mikriann@purdue.edu>
2024-11-03 13:29:33 -0500

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,3 +1,22 @@
 public interface UserInterface {
-    //
+    
+    boolean equals(Object account);
+    
+    void createPost(String title, String subtext);
+    
+    void removeFriend(User formerFriend);
+    
+    void addFriend(User newFriend);
+    
+    void block(User blockedUser);
+    
+    String getPassword();
+    
+    String getUsername();
+    
+    String getAboutMe();
+    
+    ArrayList<User> getFriendsList();
+    
+    ArrayList<User> getBlockedList();
 }
