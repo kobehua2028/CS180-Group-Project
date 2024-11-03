d590253bbb7e1fc9d49357198cf78fa8a6522df9
Michael Ikriannikov <mikriann@purdue.edu>
2024-11-03 13:32:12 -0500

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -1,3 +1,24 @@
 public interface DatabaseInterface {
-    //
+    
+    User findUser(String username);
+    
+    boolean userExists(String username);
+    
+    Post findPost(String title);
+    
+    ArrayList<User> getUsers();
+    
+    ArrayList<Post> getPosts();
+    
+    void addUser(User user);
+    
+    void addPost(Post post);
+    
+    void readUsers();
+    
+    void readPosts();
+    
+    void writeUser(User user);
+    
+    void writePost(Post post);
 }
