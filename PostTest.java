594638ee8b4cf3a64e404cc13f9d3183ae21640a
AlQarni-37 <abdulmajedalqarni212@gmail.com>
2024-11-17 20:59:11 -0500

    Add files via upload

--- PostTest.java
+++ PostTest.java
@@ -1,57 +1,21 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * CS180 Group Project
- * Program description here
- *
- * <p>Purdue University -- CS18000 -- Fall 2024</p>
- *
- * @author Kobe
- * @author Abdul
- * @version Nov 03, 2024
- */
 class PostTest {
     String testPostString = "PurduePete,Project Help,Hey guys, I'm having a hard time figuring out the project.";
 
-    private SocialMediaDatabase sm;
+    private SocialMediaDatabase sm = new SocialMediaDatabase("users.txt", "posts.txt");
 
-    private User test;
-    private Post testPost;
+    User test = new User("Beef","lYER4CAK", "AboutMe", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+            new ArrayList<>() ,sm);
+    Post testPost = new Post(test, "Test case", "Lorem Ispum", new ArrayList<Comment>(),
+            0, 0, sm);
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        sm = new SocialMediaDatabase("users.dat", "posts.dat");
-        test = null;
-        test = new User("PurduePete","lYER4CAK", "AboutMe", new ArrayList<User>(),
-                new ArrayList<User>(), sm);
-        testPost = new Post(test, "Test case", "Lorem Ispum", new ArrayList<Comment>(),
-                0, 0, sm);
-    }
 
-    @AfterEach
-    public void clearFile() throws IOException {
-        File userFile = new File("users.dat");
-        File postFile = new File("posts.dat");
-        if (userFile.exists()) {
-            userFile.delete();
-        }
-        if (postFile.exists()) {
-            postFile.delete();
-        }
-        userFile.createNewFile();
-        postFile.createNewFile();
-        this.sm = null;
-        this.test = null;
-        this.testPost = null;
-    }
 
     @Test
     public void testPost() {
@@ -143,25 +107,10 @@ class PostTest {
         assertEquals(1, testPost.getDislikes());
     }
 
-    public void testRemoveLike() {
-        testPost.removeLike();
-
-        assertEquals(0, testPost.getLikes());
-    }
-
-    @Test
-    public void testRemoveDislike() {
-
-        testPost.removeDislike();
-
-        assertEquals(0, testPost.getDislikes());
-    }
-
     @Test
     public void testEquals() {
 
-        assertEquals(true, testPost.equals(new Post(test, "Test case",
-                "Lorem Ispum", new ArrayList<Comment>(),
+        assertEquals(true, testPost.equals(new Post(test, "Test case", "Lorem Ispum", new ArrayList<Comment>(),
                 0, 1, sm)));
         assertEquals(false, testPost.equals(new Post(test, "Test casen't",
                 "Ispum Lor", new ArrayList<Comment>(), 0, 1, sm)));
