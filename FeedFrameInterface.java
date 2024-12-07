d2d69f593a42d8326cc4d87b26f7880eec4d2437
Michael Ikriannikov <mikriann@purdue.edu>
2024-12-07 14:02:01 -0800

    Update FeedFrameInterface.java

--- FeedFrameInterface.java
+++ FeedFrameInterface.java
@@ -1,3 +1,12 @@
+/**
+ * CS180 Group Project
+ * An interface for the FeedFrame
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2024</p>
+ *
+ * @author MichaelIkriannikov
+ * @version Dec 07, 2024
+ */
 public interface FeedFrameInterface {
     Panel createTopFeedPanel();
     JPanel displayComments();
