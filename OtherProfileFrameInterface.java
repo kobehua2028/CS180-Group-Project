d888c01af94e03c037b1a210c7ad0ab04b1bdd5b
AlQarni-37 <abdulmajedalqarni212@gmail.com>
2024-12-07 16:31:30 -0500

    OtherProfileFrameInterface.java

---
+++ OtherProfileFrameInterface.java
@@ -0,0 +1,13 @@
+/**
+ * Team Project -- OtherProfileFrameInterface.java
+ *
+ * This is an interface containing the methods that OtherProfileFrame.java will defiine (excluding the constructor
+ * and the run() method).
+ *
+ * @author Abdulmajed AlQarni
+ *
+ * @version December 7th, 2024
+ * */
+
+public interface OtherProfileFrameInterface {
+}
