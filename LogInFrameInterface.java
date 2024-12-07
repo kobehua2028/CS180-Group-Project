c503309d74afcb9c853a44eaafb2cd88ce35efe9
AlQarni-37 <abdulmajedalqarni212@gmail.com>
2024-12-07 16:28:20 -0500

    LoginFrameInterface.java

---
+++ LogInFrameInterface.java
@@ -0,0 +1,14 @@
+/**
+ * Team Project -- LogInFrameInterface.java
+ *
+ * This is an interface containing all the methods that will be defined in LoginFrame.java (excluding constructor
+ * and run()).
+ *
+ * @author Abdulmajed AlQarni
+ *
+ * @version December 7, 2024
+ * */
+
+public interface LogInFrameInterface {
+
+}
