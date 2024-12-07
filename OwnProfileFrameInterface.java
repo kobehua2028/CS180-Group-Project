52067478978d2a0bf58d5020f7fc6fd5566c591f
Michael Ikriannikov <mikriann@purdue.edu>
2024-12-07 14:02:47 -0800

    Update OwnProfileFrameInterface.java

--- OwnProfileFrameInterface.java
+++ OwnProfileFrameInterface.java
@@ -1,2 +1,11 @@
+/**
+ * Team Project -- OtherProfileFrameInterface.java
+ *
+ * Interface for the OwnProfileFrame class
+ *
+ * @author MichaelIkriannikov
+ *
+ * @version December 7th, 2024
+ * */
 public interface OwnProfileFrameInterface {
 }
