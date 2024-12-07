c438a1e37a1b579e8d15a362e1f58d50f99ac1ca
AlQarni-37 <abdulmajedalqarni212@gmail.com>
2024-12-07 16:34:41 -0500

    CreatePostFrameInterface.java

---
+++ CreatePostFrameInterface.java
@@ -0,0 +1,14 @@
+/**
+ * Team Project -- CreatePostFrameInterface.java
+ *
+ * This interface contains all the methods that CreatePostFrame.java will define (excluding the constructor and run()
+ * methods).
+ *
+ * @author Abdulmajed AlQarni
+ *
+ * @version December 7th, 2024
+ *
+ * */
+
+public interface CreatePostFrameInterface {
+}
